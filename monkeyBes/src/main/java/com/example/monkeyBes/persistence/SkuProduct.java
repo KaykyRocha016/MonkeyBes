30257772b9ddf9f856e5af89baf6f86bb1227f3b
FelipeMarchiC <felipe.marchi3004@gmail.co>
2025-11-11 22:13:36 -0300

    feat: add SkuProduct.java

---
+++ monkeyBes/src/main/java/com/example/monkeyBes/persistence/SkuProduct.java
@@ -0,0 +1,62 @@
+package com.example.monkeyBes.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "sku_product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SkuProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String skuCode;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column()
+    private String description;
+
+    @Column(nullable = false, precision = 4, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private int stockQuantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SkuProduct that)) return false;
+        return Objects.equals(skuCode, that.skuCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(skuCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductSku{" +
+                "id=" + id +
+                ", skuCode='" + skuCode + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", stockQuantity=" + stockQuantity +
+                '}';
+    }
+}
+
