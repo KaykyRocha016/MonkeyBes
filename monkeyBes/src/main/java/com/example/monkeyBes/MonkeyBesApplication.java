58e8d3d57ef7f485e8b0274e3e61281dc9fdd626
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-11-11 22:00:16 -0300

    springboot inititialized

---
+++ monkeyBes/src/main/java/com/example/monkeyBes/MonkeyBesApplication.java
@@ -0,0 +1,13 @@
+package com.example.monkeyBes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyBesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyBesApplication.class, args);
+	}
+
+}
