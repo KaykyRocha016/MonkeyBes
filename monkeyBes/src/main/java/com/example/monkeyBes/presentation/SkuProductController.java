50b6ae0a9eba13257ff246b508216fb0f04dcbd7
KaykyRocha016 <kaykyrocha97@gmail.com>
2025-11-11 23:26:45 -0300

    feat: implements create products

--- monkeyBes/src/main/java/com/example/monkeyBes/presentation/SkuProductController.java
+++ monkeyBes/src/main/java/com/example/monkeyBes/presentation/SkuProductController.java
@@ -3,10 +3,7 @@ package com.example.monkeyBes.presentation;
 import com.example.monkeyBes.persistence.model.SkuProduct;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.monkeyBes.service.Implementation.ItemService;
 import com.example.monkeyBes.service.Implementation.ProductService;
 import com.example.monkeyBes.service.Implementation.StockService;
@@ -40,15 +37,11 @@ public class SkuProductController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    // 🔹 Criar novo SKU
-//    @PostMapping
-//    public ResponseEntity<ProductSku> create(@RequestBody ProductSku sku) {
-//        ProductSku saved = service.create(sku);
-//        URI location = URI.create("/api/skus/" + saved.getId());
-//        return ResponseEntity.created(location).body(saved);
-//    }
-//
-//    // 🔹 Atualizar SKU existente
+    @PostMapping
+    public ResponseEntity<SkuProduct> create(@RequestBody SkuProduct product) {
+        productService.create(product);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 //    @PutMapping("/{id}")
 //    public ResponseEntity<ProductSku> update(@PathVariable Long id, @RequestBody ProductSku sku) {
 //        return service.update(id, sku)
