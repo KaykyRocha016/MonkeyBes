c36d9fa3a4ecc8a53560262525953cf85cb20ece
Turazzi <analivia.turazzi@gmail.com>
2025-11-18 21:02:30 -0300

    feat: Adds method to show most and least sold products

--- monkeyBes/src/main/java/com/example/monkeyBes/persistence/access/ItemRepository.java
+++ monkeyBes/src/main/java/com/example/monkeyBes/persistence/access/ItemRepository.java
@@ -2,8 +2,24 @@ package com.example.monkeyBes.persistence.access;
 
 import com.example.monkeyBes.persistence.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<Item, UUID> {
+     //Busca os produtos mais vendidos
+    @Query("SELECT i.product AS product, SUM(i.quantity) AS totalSold " +
+            "FROM Item i " +
+            "GROUP BY i.product " +
+            "ORDER BY totalSold DESC")
+    List<Object[]> findMostSoldProducts();
+
+    //Busca os menos vendidos
+    @Query("SELECT i.product AS product, SUM(i.quantity) AS totalSold " +
+            "FROM Item i " +
+            "GROUP BY i.product " +
+            "ORDER BY totalSold ASC")
+    List<Object[]> findLastSoldProducts();
+
 }
